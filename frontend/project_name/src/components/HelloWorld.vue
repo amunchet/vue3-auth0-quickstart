ede73e33b2f326f952976bee481fff64caf38414
Chester Enright <amunchet@gmail.com>
2022-09-30 17:11:51 -0500

    chore: update to have Auth0

--- frontend/project_name/src/components/HelloWorld.vue
+++ frontend/project_name/src/components/HelloWorld.vue
@@ -1,12 +1,16 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 :class="isAuthenticated ? 'green' : ''">{{ isAuthenticated ? 'User logged in' : '-'}}</h1>
+    
+    <div class="border">
+    <div  v-if="isAuthenticated">
+      User: {{user.name}} <br />
+      <img :src="user.picture" alt="User Profile" height="100px" />
+      <hr />
+      <button class="mt-2" @click="logOut">Log out</button>
+    </div>
+    <button v-else class="mt-2" @click="login">Log in</button>
+    </div>
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
@@ -14,6 +18,14 @@ defineProps<{
     </h3>
   </div>
 </template>
+
+<script setup lang="ts">
+    import { useAuth0 } from '@auth0/auth0-vue';
+    const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+    const login = ()=>loginWithRedirect();
+    const logOut = ()=>logout({returnTo: window.location.origin});
+    
+</script>
 
 <style scoped>
 h1 {
@@ -26,6 +38,10 @@ h3 {
   font-size: 1.2rem;
 }
 
+.mt-2{
+  margin-top: 1rem;
+}
+
 .greetings h1,
 .greetings h3 {
   text-align: center;
@@ -36,5 +52,12 @@ h3 {
   .greetings h3 {
     text-align: left;
   }
+}
+
+.border{
+  border: 1px solid #65656d;
+  padding: 0.5rem;
+  margin: 0.25rem;
+  border-radius: 0.25rem;
 }
 </style>
--- frontend/project_name/src/main.ts
+++ frontend/project_name/src/main.ts
@@ -4,8 +4,19 @@ import router from './router'
 
 import './assets/main.css'
 
+import authConfig from './auth_config.json'
+import { createAuth0 } from '@auth0/auth0-vue';
+
 const app = createApp(App)
 
 app.use(router)
+
+app.use(
+    createAuth0({
+      domain: authConfig.domain,
+      client_id: authConfig.clientId,
+      redirect_uri: window.location.origin
+    })
+  );
 
 app.mount('#app')
